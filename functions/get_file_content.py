7f5fca4514f7a8d40d71510557154b7c55ad8569
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-15 07:23:49 +0200

    reads file in memory and shortens if necesarry

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,15 +1,19 @@
 import pathlib
 import os
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
-    print(working_directory) # DEBUG
-    print(file_path) # DEBUG
-    print(os.path.abspath(file_path))
-    if not os.path.abspath(file_path).startswith(working_directory):
+    abs_file_path = os.path.abspath(file_path)
+    if not abs_file_path.startswith(working_directory):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    elif not os.path.isfile(file_path):
+    elif not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    return "all good" # DEBUG
-    
 
+    with open(abs_file_path, "r") as f:
+        file_content = f.read(MAX_CHARS)
+        if os.path.getsize(abs_file_path) > MAX_CHARS:
+            file_content += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content
+    
+        
 print(get_file_content(os.getcwd(), 'file.txt')) 
