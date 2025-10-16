bd5e3e1211e6a410697a0313cabdf92dd5bd6952
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-16 20:13:48 +0200

    Function calling function decalaration

--- tests.py
+++ tests.py
@@ -1,13 +1,22 @@
-from functions.run_python_file import run_python_file
+from functions.run_python import run_python_file
 
 
 def test():
-    print(run_python_file("calculator", "main.py"))
-    print(run_python_file("calculator", "main.py", ["3 + 5"]))
-    print(run_python_file("calculator", "tests.py"))
-    print(run_python_file("calculator", "../main.py"))
-    print(run_python_file("calculator", "nonexistent.py"))
-    print(run_python_file("calculator", "lorem.txt"))
+    result = run_python_file("calculator", "main.py")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+
+    result = run_python_file("calculator", "lorem.txt")
+    print(result)
+
 
 if __name__ == "__main__":
     test()
