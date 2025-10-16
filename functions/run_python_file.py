4ac228b8dcad2a64e9e31114ab327cdd6aa6a02a
Wet-Senker <dhrnickdevries@hotmail.com>
2025-10-16 08:59:08 +0200

    added two return values to run_python_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -7,9 +7,9 @@ def run_python_file(working_directory, file_path, args=[]):
     if not abs_file_path.startswith(abs_cwd):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(abs_file_path):
-        f'Error: File "{file_path}" not found.'
+        return f'Error: File "{file_path}" not found.'
     if not abs_file_path.endswith(".py"):
-        f'Error: "{file_path}" is not a Python file.'
+        return f'Error: "{file_path}" is not a Python file.'
 
     try:
         commands = ["python", abs_file_path]
